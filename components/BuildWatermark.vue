48a57379b6d938e5267cf9bcea2a54c4af28cf99
Amrithanath M <amrithanadhm@gmail.com>
2026-06-22 15:37:04 +0530

    update build info

--- components/BuildWatermark.vue
+++ components/BuildWatermark.vue
@@ -2,8 +2,8 @@
   <div class="fixed bottom-6 right-4 z-[9999] pointer-events-none select-none opacity-30">
     <div class="text-right">
       <p class="text-sm font-bold text-gray-800 tracking-wide">LobbyBee Test Build</p>
-      <p class="text-xs text-gray-600">Frontend: v09042026</p>
-      <p class="text-xs text-gray-600">Backend: v29032026</p>
+      <p class="text-xs text-gray-600">Frontend: v20062026</p>
+      <p class="text-xs text-gray-600">Backend: v20062026</p>
     </div>
   </div>
 </template>
